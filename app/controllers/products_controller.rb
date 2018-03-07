905414987d41168a1c72e3f0fbbcb9b17294a89a
Jennifer Blom <jennifer.blom@betterment.com>
2018-03-06 20:46:38 -0500

    add controller and routes for single and all products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def one_product
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def all_products
+    products = Product.all
+    render json: products.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+  get '/product' => 'products#one_product'
+  get '/all_products' => 'products#all_products'
 end
