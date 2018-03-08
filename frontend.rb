bd1e8f5b3f3d34a79d744f71176bd110a6245f20
Jennifer Blom <jennifer.blom@betterment.com>
2018-03-07 19:39:39 -0500

    add choice to frontend

--- frontend.rb
+++ frontend.rb
@@ -1,22 +1,33 @@
 require 'unirest'
 require 'tty-table'
 
-response = Unirest.get("localhost:3000/product")
-product = response.body
+base_url = "localhost:3000/v1"
 
-# p "Name: #{product['name']}"
-# p "Price: #{product['price']}"
-# p "Description: #{product['description']}"
+p "Please select what you would like to see:"
+p "[1] See all products"
+p "[2] See one product"
+user_input = gets.chomp
 
-table = TTY::Table.new ['Name','Price', 'Description'], [["#{product['name']}", "#{product['price']}", "#{product['description']}"]]
-table.render(:basic)
+if user_input == "2"
+  response = Unirest.get("#{base_url}/product")
+  product = response.body
 
-# all_response = Unirest.get("localhost:3000/all_products")
-# products = all_response.body
+  p "Name: #{product['name']}"
+  p "Price: #{product['price']}"
+  p "Description: #{product['description']}"
 
-# products.each do |product|
-#   p "Name: #{product['name']}"
-#   p "Price: #{product['price']}"
-#   p "Description: #{product['description']}"
-#   p "-" * 30
-# end
+# table = TTY::Table.new ['Name','Price', 'Description'], [["#{product['name']}", "#{product['price']}", "#{product['description']}"]]
+# table.render(:basic)
+
+elsif user_input == "1"
+
+  all_response = Unirest.get("#{base_url}/all_products")
+  products = all_response.body
+
+  products.each do |product|
+    p "Name: #{product['name']}"
+    p "Price: #{product['price']}"
+    p "Description: #{product['description']}"
+    p "-" * 30
+  end
+end
