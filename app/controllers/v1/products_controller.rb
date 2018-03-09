d7efa243f4ea52f4fefb88406a9d01313b06d138
Jennifer Blom <jennifer.blom@betterment.com>
2018-03-08 20:37:47 -0500

    make restful

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,12 +1,12 @@
 class V1::ProductsController < ApplicationController
-  def one_product
-    id = params["id"].to_i
-    product = Product.find(id)
-    render json: product.as_json
-  end
-
-  def all_products
+  def index
     products = Product.all
     render json: products.as_json
+  end
+
+  def show
+    id = params[:id]
+    product = Product.find(id)
+    render json: product.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    get '/product/:id' => 'products#one_product'
-    get '/product' => 'products#one_product'
-    get '/all_products' => 'products#all_products'
+    get '/products' => 'products#index'
+    get '/products/:id' => 'products#show'
   end
 end
