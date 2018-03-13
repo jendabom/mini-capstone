a5df9f5b174979068279e0266125df6731f696c8
Jennifer Blom <jennifer.blom@betterment.com>
2018-03-12 20:42:03 -0400

    override as_json and add methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,26 @@
 class Product < ApplicationRecord
+  def as_json
+    {
+      id: id,
+      name: name,
+      price: price,
+      image_url: image_url,
+      description: description,
+      discounted: is_discounted?, 
+      total: total
+    }
+  end
+
+  def is_discounted?
+    price < 3
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    sum = price + tax
+    sum.round(2)
+  end
 end
