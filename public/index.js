09b7820fcc6f06705b4daf47b2f6a903d2a1fe6d
Jennifer Blom <jennifer.blom@betterment.com>
2018-04-16 21:57:27 -0400

    add frontend authentication and vue-js

--- public/index.js
+++ public/index.js
@@ -1,11 +1,133 @@
-console.log('the js file is being loaded correctly');
+/* global Vue, VueRouter, axios */
 
-var productTemplate = document.querySelector('#product-card');
-var productContainer = document.querySelector('.row');
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!", 
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("/v1/products").then(function(response) {
+      this.products = response.data;
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
 
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
+var PracticePage = {
+  template: "#practice-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!", 
+    };
+  },
+  created: function() {
+    axios.get("/v1/products").then(function(response) {
+      this.products = response.data;
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
+
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/v1/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LogoutPage = {
+  template: "<h1>Logout</h1>",
+  created: function() {
+    axios.defaults.headers.common["Authorization"] = undefined;
+    localStorage.removeItem("jwt");
+    router.push("/");
+  }
+};
+
+var router = new VueRouter({
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/practice", component: PracticePage },
+    { path: "/signup", component: SignupPage },
+    { path: "/login", component: LoginPage },
+    { path: "/logout", component: LogoutPage }
+  ],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router,
+  created: function() {
+    var jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      axios.defaults.headers.common["Authorization"] = jwt;
+    }
+  }
+});
